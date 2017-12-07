7453bf0728a005e8a3a8fb457e950075a913d7fa
kelly quinn <kellyquinn213@gmail.com>
2017-12-07 00:23:21 -0500

    connection and gitignore

---
+++ config/connection.js
@@ -0,0 +1,31 @@
+//MySQL connection.
+var mysql = require("mysql");
+
+// JAWSdb
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+// Local connection
+  var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    //enter user
+    user: "",
+    //enter password
+    password: "",
+    //create database
+    database: ""
+  });
+}
+
+//Connection
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+//Export connection for the ORM to use
+module.exports = connection;
