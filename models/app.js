39ea81928bd69437b282fd0ebf46deb35132682b
Mohammed Wardeh <wardeh@un.org>
2017-12-13 00:15:54 -0500

    Update app.js

--- models/app.js
+++ models/app.js
@@ -2,28 +2,44 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-  all: function (cb) {
+  all: function(cb) {
+    orm.all("all_patients", function(res) {
+      cb(res);
+    });
+  },
+  
+  // The variables cols and vals are arrays.
 
-    orm.all("all_patients", function (res) {
-
+  createDoc: function(cols, vals, cb) {
+    orm.createDoc("new_doctor", cols, vals, function(res) {
       cb(res);
     });
   },
 
-  createDoc: function (vals, cb) {
-    orm.createDoc("new_doctor", vals, function (res) {
+
+
+  createPatient: function(cols, vals, cb) {  
+    orm.createPatient("new_patient", cols, vals, function(res) {
       cb(res);
     });
   },
 
-  createPatient: function (cols, vals, cb) {
 
-    orm.createPatient("new_patient", cols, vals, function (res) {
+
+  update: function(objColVals, condition, cb) {
+    orm.update("patient_vu", objColVals, condition, function(res) {
       cb(res);
     });
-  }
+  },
 
+
+  searchPatient: function(vals, cb) {
+    console.log("app.js " + vals);  
+        orm.searchPatient("find_patient", vals, function(res) {
+          cb(res);
+        });
+      }
 };
 
-// Export the database functions for the controller (burgers_controller.js).
+
 module.exports = burger;
