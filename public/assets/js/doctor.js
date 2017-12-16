ae4646bd726f05569bb22f3e1a509552ba87bf63
Scot Renz <scotrenz@gmail.com>
2017-12-16 10:38:24 -0500

    before present

--- public/assets/js/doctor.js
+++ public/assets/js/doctor.js
@@ -66,11 +66,11 @@ $(function () {
     $("#update_patient_info").on("click", function (e) {
         e.preventDefault();
         var patientID = $("#patientID").attr("value");
-
+        console.log($("#DOB").attr("value"))
         var newRecord = {
             First_Name: $("#patient_firstName").attr("value"),
             Last_Name: $("#patient_LASTname").attr("value"),
-            // DOB: $("#DOB").attr("value"),
+            DOB: $("#DOB").attr("value"),
             DOB: "1990/12/12",
             doctorsID: $("#docID").attr("value"),
             Street_Address: $("#patient_address").attr("value"),
